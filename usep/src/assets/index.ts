5d72af8e6eb97e9dcc82f9958e8d80404367df98
maureyesoro <mauricio.reyes@enroutesystems.com>
2021-12-17 18:34:34 -0600

    dirs created layout done

---
+++ usep/src/assets/index.ts
@@ -0,0 +1,11 @@
+import { ReactComponent as University } from "./svg/university.svg";
+// import InstagramLogo from "./png/instagram.png";
+
+
+
+const images = {
+    University
+    // put hear all imported images
+};
+
+export default images
